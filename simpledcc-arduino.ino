770ae8c7135a525556586006c5469096e111312c
Loco Team <hey@loco.engineering>
2024-05-22 16:08:41 +0200

    Add audio and spiffs modules

--- simpledcc-arduino.ino
+++ simpledcc-arduino.ino
@@ -1,4 +1,5 @@
 #include "SPIFFS.h"
+#include "FS.h"
 #include "config.h"
 #include "structures.h"
 
@@ -7,6 +8,8 @@
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
 #include <PCA9955B.h>
+
+#include "driver/i2s.h"
 
 #include <SPI.h>
 #include <MFRC522v2.h>
@@ -18,6 +21,8 @@
 #include "src/dcc_reader/dcc_module.h"
 #include "src/features/led_module.h"
 #include "src/features/nfc_module.h"
+#include "src/features/audio_module.h"
+#include "src/features/spiffs_module.h"
 
 // Initialize SPIFFS
 void initSPIFFS()
@@ -43,10 +48,15 @@ void setup()
   initSPIFFS();
   setup_webserver();
   setup_led();
-  setup_nfc();
+  //setup_nfc();
   // LED connection examples for a level crossing with 2 LEDs blinking alternately
   //add_led_connection(1, 1, 0.5, 1000, 1000, 0);
   //add_led_connection(1, 2, 0.5, 1000, 1000, 1000);
+
+  listDir(SPIFFS, "/", 0);
+
+  //Test audio
+  play_audio();
 }
 
 void loop()
@@ -54,5 +64,6 @@ void loop()
   loop_dcc_module();
   loop_webserver();
   loop_led();
-  loop_nfc();
+  //loop_nfc();
+  loop_audio();
 }
---
+++ src/features/audio_module.h
@@ -0,0 +1,181 @@
+
+#include "test_audio.h"    // The Wav file
+
+static const i2s_port_t i2s_num = I2S_NUM_0; // i2s port number
+unsigned const char *wav_data;
+uint32_t wav_data_index = 0; // index offset into "wav_data" for current  data t send to I2S
+
+struct WavHeader_Struct
+{
+    //   RIFF Section
+    char RIFFSectionID[4]; // Letters "RIFF"
+    uint32_t Size;         // Size of entire file less 8
+    char RiffFormat[4];    // Letters "WAVE"
+
+    //   Format Section
+    char FormatSectionID[4]; // letters "fmt"
+    uint32_t FormatSize;     // Size of format section less 8
+    uint16_t FormatID;       // 1=uncompressed PCM
+    uint16_t NumChannels;    // 1=mono,2=stereo
+    uint32_t SampleRate;     // 44100, 16000, 8000 etc.
+    uint32_t ByteRate;       // =SampleRate * Channels * (BitsPerSample/8)
+    uint16_t BlockAlign;     // =Channels * (BitsPerSample/8)
+    uint16_t BitsPerSample;  // 8,16,24 or 32
+
+    // Data Section
+    char DataSectionID[4]; // The letters "data"
+    uint32_t DataSize;     // Size of the data that follows
+} WavHeader;
+
+static const i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = 44100,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+    .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
+    .dma_buf_count = 8,                       // 8 buffers
+    .dma_buf_len = 1024,                      // 1K per buffer, so 8K of buffer space
+    .use_apll = 0,
+    .tx_desc_auto_clear = true,
+    .fixed_mclk = -1};
+
+static const i2s_pin_config_t pin_config = {
+    .bck_io_num = 13,                // The bit clock connectiom, goes to pin 27 of ESP32
+    .ws_io_num = 14,                 // Word select, also known as word select or left right clock
+    .data_out_num = 12,           // Data out from the ESP32, connect to DIN on 38357A
+    .data_in_num = I2S_PIN_NO_CHANGE // we are not interested in I2S data into the ESP32
+};
+
+void PrintData(const char *Data, uint8_t NumBytes)
+{
+    for (uint8_t i = 0; i < NumBytes; i++)
+        Serial.print(Data[i]);
+    Serial.println();
+}
+
+bool ValidWavData(WavHeader_Struct *Wav)
+{
+
+    if (memcmp(Wav->RIFFSectionID, "RIFF", 4) != 0)
+    {
+        Serial.print("Invlaid data - Not RIFF format");
+        return false;
+    }
+    if (memcmp(Wav->RiffFormat, "WAVE", 4) != 0)
+    {
+        Serial.print("Invlaid data - Not Wave file");
+        return false;
+    }
+    if (memcmp(Wav->FormatSectionID, "fmt", 3) != 0)
+    {
+        Serial.print("Invlaid data - No format section found");
+        return false;
+    }
+    if (memcmp(Wav->DataSectionID, "data", 4) != 0)
+    {
+        Serial.print("Invlaid data - data section not found");
+        return false;
+    }
+    if (Wav->FormatID != 1)
+    {
+        Serial.print("Invlaid data - format Id must be 1");
+        return false;
+    }
+    if (Wav->FormatSize != 16)
+    {
+        Serial.print("Invlaid data - format section size must be 16.");
+        return false;
+    }
+    if ((Wav->NumChannels != 1) & (Wav->NumChannels != 2))
+    {
+        Serial.print("Invlaid data - only mono or stereo permitted.");
+        return false;
+    }
+    if (Wav->SampleRate > 48000)
+    {
+        Serial.print("Invlaid data - Sample rate cannot be greater than 48000");
+        return false;
+    }
+    if ((Wav->BitsPerSample != 8) & (Wav->BitsPerSample != 16))
+    {
+        Serial.print("Invlaid data - Only 8 or 16 bits per sample permitted.");
+        return false;
+    }
+    return true;
+}
+
+void DumpWAVHeader(WavHeader_Struct *Wav)
+{
+    if (memcmp(Wav->RIFFSectionID, "RIFF", 4) != 0)
+    {
+        Serial.print("Not a RIFF format file - ");
+        PrintData(Wav->RIFFSectionID, 4);
+        return;
+    }
+    if (memcmp(Wav->RiffFormat, "WAVE", 4) != 0)
+    {
+        Serial.print("Not a WAVE file - ");
+        PrintData(Wav->RiffFormat, 4);
+        return;
+    }
+    if (memcmp(Wav->FormatSectionID, "fmt", 3) != 0)
+    {
+        Serial.print("fmt ID not present - ");
+        PrintData(Wav->FormatSectionID, 3);
+        return;
+    }
+    if (memcmp(Wav->DataSectionID, "data", 4) != 0)
+    {
+        Serial.print("data ID not present - ");
+        PrintData(Wav->DataSectionID, 4);
+        return;
+    }
+    // All looks good, dump the data
+    Serial.print("Total size :");
+    Serial.println(Wav->Size);
+    Serial.print("Format section size :");
+    Serial.println(Wav->FormatSize);
+    Serial.print("Wave format :");
+    Serial.println(Wav->FormatID);
+    Serial.print("Channels :");
+    Serial.println(Wav->NumChannels);
+    Serial.print("Sample Rate :");
+    Serial.println(Wav->SampleRate);
+    Serial.print("Byte Rate :");
+    Serial.println(Wav->ByteRate);
+    Serial.print("Block Align :");
+    Serial.println(Wav->BlockAlign);
+    Serial.print("Bits Per Sample :");
+    Serial.println(Wav->BitsPerSample);
+    Serial.print("Data Size :");
+    Serial.println(Wav->DataSize);
+}
+
+
+void play_audio()
+{
+    memcpy(&WavHeader, &wav_file_content, 44); // Copy the header part of the wav data into our structure
+    DumpWAVHeader(&WavHeader);                 // Dump the header data to serial, optional!
+    if (ValidWavData(&WavHeader))
+    {
+        i2s_driver_install(i2s_num, &i2s_config, 0, NULL);   // ESP32 will allocated resources to run I2S
+        i2s_set_pin(i2s_num, &pin_config);                   // Tell it the pins you will be using
+        i2s_set_sample_rates(i2s_num, WavHeader.SampleRate); // set sample rate
+        wav_data = wav_file_content;                         // set to start of data
+        wav_data += 44;
+        wav_data_index = 0;
+    }
+}
+
+void loop_audio()
+{
+
+    size_t BytesWritten; // Returned by the I2S write routine, we are not interested in it
+
+    if (wav_data_index < WavHeader.DataSize)
+    {
+        i2s_write(i2s_num, wav_data + wav_data_index, 4, &BytesWritten, portMAX_DELAY);
+        wav_data_index += 4;
+    }
+}
